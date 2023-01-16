1669acebb2fdfdb4475bca696f7a8447b2a39fad
Kirill Taran <kirilltaran@hotmail.com>
2023-01-17 00:03:29 +0400

    Contracts updated

--- services/artists/converters/release_conversion_service.go
+++ services/artists/converters/release_conversion_service.go
@@ -35,11 +35,11 @@ func ToReleaseMap(release *contracts.Release) map[string]any {
 	return map[string]any{
 		"PageTitle":          title,
 		"ArtistNames":        release.ReleaseArtists,
-		"Description":        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		"Description":        release.Description,
 		"ReleaseName":        release.Name,
 		"ReleaseDate":        release.ReleaseDate.AsTime().Year(),
 		"ImageDetails":       toImageDetailsMap(release.ImageDetails),
-		"Tags":               []string{"indie", "post-punk"},
+		"Tags":               release.Tags,
 		"Tracks":             tracks,
 		"StreamingPlatforms": toPlatformUrlMaps(release.PlatformUrls),
 	}
