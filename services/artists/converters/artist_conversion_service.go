d462d663a882bd549e08ce8ab92a913d6c31eec9
Kirill Taran <kirilltaran@hotmail.com>
2023-05-26 18:50:58 +0400

    A name added to the artist page according to the new layout

--- services/artists/converters/artist_conversion_service.go
+++ services/artists/converters/artist_conversion_service.go
@@ -8,9 +8,10 @@ import (
 
 func ToArtistMap(hashCoder commonServices.HashCoder, dataService commonServices.TemplateDataServer, artist *contracts.Artist, sortedReleases []*contracts.SlimRelease) map[string]any {
 	return dataService.Enrich(artist.Name, map[string]any{
-		"Slug":               hashCoder.Encode(int(artist.Id)),
-		"AlbumNumber":        int(artist.ReleaseStats.AlbumNumber),
-		"Name":               artist.Name,
+		//"Slug":               hashCoder.Encode(int(artist.Id)),
+		"Artists":     toSlimArtistMaps(hashCoder, artist),
+		"AlbumNumber": int(artist.ReleaseStats.AlbumNumber),
+		//"Name":               artist.Name,
 		"CompilationNumber":  int(artist.ReleaseStats.CompilationNumber),
 		"PresentationConfig": artist.PresentationConfig,
 		"Releases":           ToSlimReleaseMaps(hashCoder, sortedReleases),
@@ -22,15 +23,21 @@ func ToArtistMap(hashCoder commonServices.HashCoder, dataService commonServices.
 func ToSlimArtistMaps(hashCoder commonServices.HashCoder, artists []*contracts.SlimArtist) []map[string]any {
 	results := make([]map[string]any, len(artists))
 	for i, artist := range artists {
-		results[i] = toSlimArtistMap(hashCoder, artist)
+		results[i] = toSlimArtistMap(hashCoder, artist.Id, artist.Name)
 	}
 
 	return results
 }
 
-func toSlimArtistMap(hashCoder commonServices.HashCoder, artist *contracts.SlimArtist) map[string]any {
+func toSlimArtistMaps(hashCoder commonServices.HashCoder, artist *contracts.Artist) []map[string]any {
+	slimArtistMap := toSlimArtistMap(hashCoder, artist.Id, artist.Name)
+
+	return []map[string]any{slimArtistMap}
+}
+
+func toSlimArtistMap(hashCoder commonServices.HashCoder, id int32, name string) map[string]any {
 	return map[string]any{
-		"Slug": hashCoder.Encode(int(artist.Id)),
-		"Name": artist.Name,
+		"Slug": hashCoder.Encode(int(id)),
+		"Name": name,
 	}
 }
